7d15282ef6f74e4d8bab4535b99c538640b68ef4
Jiun-Chih Pan <jimmypan@gmail.com>
2021-03-20 12:57:15 -0400

    Test environment variable

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    // console.log('Containers rule!');
+    const favoriteFood = process.env.FAVORITE_FOOD
+    console.log(`The env variable FAVORITE_FOOD = ${favoriteFood}`);
     await sleep(5000);
   }
 }
